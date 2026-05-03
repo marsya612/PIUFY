d6563caaa0e83de9ab4abfb841ded2f67a88749b
Marsyanda <97005301+marsya612@users.noreply.github.com>
2026-05-03 09:20:56 +0700

    Add Http and URL facades to AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\URL;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 
