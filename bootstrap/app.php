21ab614dab063f71f982127adc782b6692489322
Marsyanda <97005301+marsya612@users.noreply.github.com>
2026-05-09 16:43:42 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,27 @@
-<?php
 
+
+// use Illuminate\Foundation\Application;
+// use Illuminate\Foundation\Configuration\Exceptions;
+// use Illuminate\Foundation\Configuration\Middleware;
+
+// return Application::configure(basePath: dirname(__DIR__))
+//     ->withRouting(
+//         web: __DIR__.'/../routes/web.php',
+//         commands: __DIR__.'/../routes/console.php',
+//         health: '/up',
+//     )
+//     ->withMiddleware(function (Middleware $middleware): void {
+//         //
+//     })
+//     ->withExceptions(function (Exceptions $exceptions): void {
+//         //
+//     })
+//     ->create();
+
+<?php
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -11,8 +29,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
